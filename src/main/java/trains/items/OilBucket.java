f54c182458f8aa5da87f8eb6c8bd15a6836d87ee
MisterJW123 <ralphthedoge@gmail.com>
2016-07-06 16:59:56 -0400

    Improved oil kinda

--- src/main/java/trains/items/OilBucket.java
+++ src/main/java/trains/items/OilBucket.java
@@ -28,7 +28,25 @@ public class OilBucket extends Item{
 	@Override
 	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int posX, int posY, int posZ, int blockSide, float pointToRayX, float pointToRayY, float pointToRayZ){
 		//TODO Find the face of the block pointed at
-		world.setBlock(posX, posY + 1, posZ, fluid);
+		switch(blockSide){
+		case 0:
+			world.setBlock(posX, posY - 1, posZ, fluid);
+			break;
+		case 1:
+			world.setBlock(posX, posY + 1, posZ, fluid);
+			break;
+		case 2:
+			world.setBlock(posX, posY, posZ - 1, fluid);
+			break;
+		case 3:
+			world.setBlock(posX, posY, posZ + 1, fluid);
+		case 4:
+			world.setBlock(posX - 1, posY, posZ, fluid);
+		case 5:
+			world.setBlock(posX + 1, posY, posZ, fluid);
+		default:
+			world.setBlock(posX, posY, posZ, fluid);
+		}
 		
 		return false;
 		
